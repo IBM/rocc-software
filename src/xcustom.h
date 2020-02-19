bf45964146243f47551719fb949ef7dbcc0f6241
Pranav Prakash <pranav-prakash@users.noreply.github.com>
2020-02-18 22:35:53 -0800

    Update instruction macros to use .insn directive
    The prior solution relied on deprecated register storage class specifiers.
    Note that .insn directives require a sufficiently recent version of binutils.
    
    Addresses issue #3.
    
    DCO 1.1 Signed-off-by: Pranav Prakash <pranav-prakash@users.noreply.github.com>

--- src/xcustom.h
+++ src/xcustom.h
@@ -19,62 +19,32 @@
 #ifndef STR
 #define STR(x) STR1(x)
 #endif
-#define EXTRACT(a, size, offset) (((~(~0 << size) << offset) & a) >> offset)
 
-// rd = rs2[offset + size - 1 : offset]
-// rs1 is clobbered
-// rs2 is left intact
-#define EXTRACT_RAW(rd, rs1, rs2, size, offset) \
-  not x ## rs1, x0;                             \
-  slli x ## rs1, x ## rs1, size;                \
-  not x ## rs1, x ## rs1;                       \
-  slli x ## rs1, x ## rs1, offset;              \
-  and x ## rd, x ## rs1, x ## rs2;              \
-  srai x ## rd, x ## rd, offset;
-
-#define XCUSTOM_OPCODE(x) XCUSTOM_OPCODE_ ## x
+#define XCUSTOM_OPCODE(x) XCUSTOM_OPCODE_##x
 #define XCUSTOM_OPCODE_0 0b0001011
 #define XCUSTOM_OPCODE_1 0b0101011
 #define XCUSTOM_OPCODE_2 0b1011011
 #define XCUSTOM_OPCODE_3 0b1111011
 
-#define XCUSTOM(x, rd, rs1, rs2, funct)         \
-  XCUSTOM_OPCODE(x)                   |         \
-  (rd                   << (7))       |         \
-  (0x3                  << (7+5))     |         \
-  ((rd != 0) & 1        << (7+5+2))   |         \
-  (rs1                  << (7+5+3))   |         \
-  (rs2                  << (7+5+3+5)) |         \
-  (EXTRACT(funct, 7, 0) << (7+5+3+5+5))
-
-#define ROCC_INSTRUCTION_RAW_R_R_R(x, rd, rs1, rs2, funct)      \
-  .word XCUSTOM(x, ## rd, ## rs1, ## rs2, funct)
-
 // Standard macro that passes rd, rs1, and rs2 via registers
-#define ROCC_INSTRUCTION(x, rd, rs1, rs2, funct)                \
-  ROCC_INSTRUCTION_R_R_R(x, rd, rs1, rs2, funct, 10, 11, 12)
+#define ROCC_INSTRUCTION(x, rd, rs1, rs2, funct7) \
+  ROCC_INSTRUCTION_R_R_R(x, rd, rs1, rs2, funct7)
 
 // rd, rs1, and rs2 are data
-// rd_n, rs1_n, and rs2_n are the register numbers to use
-#define ROCC_INSTRUCTION_R_R_R(x, rd, rs1, rs2, funct, rd_n, rs1_n, rs2_n) \
-  {                                                                     \
-    register uint64_t rd_  asm ("x" # rd_n);                            \
-    register uint64_t rs1_ asm ("x" # rs1_n) = (uint64_t) rs1;          \
-    register uint64_t rs2_ asm ("x" # rs2_n) = (uint64_t) rs2;          \
-    asm volatile (                                                      \
-        ".word " STR(XCUSTOM(x, rd_n, rs1_n, rs2_n, funct)) "\n\t"      \
-        : "=r" (rd_)                                                    \
-        : [_rs1] "r" (rs1_), [_rs2] "r" (rs2_));                        \
-    rd = rd_;                                                           \
+#define ROCC_INSTRUCTION_R_R_R(x, rd, rs1, rs2, funct7)                                 \
+  {                                                                                     \
+    asm volatile(                                                                       \
+        ".insn r " STR(XCUSTOM_OPCODE(x)) ", " STR(0x3) ", " STR(funct7) ", %0, %1, %2" \
+        : "=r"(rd)                                                                      \
+        : "r"(rs1), "r"(rs2));                                                          \
   }
 
-#define ROCC_INSTRUCTION_0_R_R(x, rs1, rs2, funct, rs1_n, rs2_n)  \
-  {                                                               \
-    register uint64_t rs1_ asm ("x" # rs1_n) = (uint64_t) rs1;    \
-    register uint64_t rs2_ asm ("x" # rs2_n) = (uint64_t) rs2;    \
-    asm volatile (                                                \
-        ".word " STR(XCUSTOM(x, 0, rs1_n, rs2_n, funct)) "\n\t"   \
-        :: [_rs1] "r" (rs1_), [_rs2] "r" (rs2_));                 \
+#define ROCC_INSTRUCTION_0_R_R(x, rs1, rs2, funct7)                                     \
+  {                                                                                     \
+    asm volatile(                                                                       \
+        ".insn r " STR(XCUSTOM_OPCODE(x)) ", " STR(0x3) ", " STR(funct7) ", x0, %0, %1" \
+        :                                                                               \
+        : "r"(rs1), "r"(rs2));                                                          \
   }
 
 // [TODO] fix these to align with the above approach
